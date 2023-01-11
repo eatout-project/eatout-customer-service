e8cfb0a1d096f7c36979f938bbf1f2ea0295ee93
chma <christian.em@live.dk>
2023-01-11 16:01:09 +0100

    presentation changes

--- index.ts
+++ index.ts
@@ -10,18 +10,17 @@ dotenv.config();
 const app: Express = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port = process.env.PORT;
+const port = process.env.PORT || 5004;
 
 const db = knex({
     client: 'mysql2',
     connection: {
-        host: `${process.env.CUSTOMER_LOGIN_DB_HOST}`,
-        port: parseInt(`${process.env.CUSTOMER_LOGIN_DB_PORT}`),
-        user: `${process.env.CUSTOMER_LOGIN_DB_USER}`,
-        database: `${process.env.CUSTOMER_LOGIN_DB}`
+        host: process.env.CUSTOMER_LOGIN_DB_HOST ? `${process.env.CUSTOMER_LOGIN_DB_HOST}` : `127.0.0.1`,
+        port: process.env.CUSTOMER_LOGIN_DB_PORT ? parseInt(`${process.env.CUSTOMER_LOGIN_DB_PORT}`) : 3310,
+        user: process.env.CUSTOMER_LOGIN_DB_USER ? `${process.env.CUSTOMER_LOGIN_DB_USER}` : `root`,
+        database: process.env.CUSTOMER_LOGIN_DB ? `${process.env.CUSTOMER_LOGIN_DB}` : `db`
     }
 });
-
 
 app.post('/create-account', (req: Request, res: Response) => createAccount(req, res, db));
 
@@ -30,5 +29,5 @@ app.post('/login', (req: Request, res: Response) => login(req, res, db));
 app.post('/verifyAccount', (req: Request, res: Response) => verifyAccount(req, res, db));
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 });
